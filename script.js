da77e5c00b4934a5917442b2e3976f89fa2bab58
K3ow <93838411+K3ow@users.noreply.github.com>
2021-11-07 04:00:51 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+function aorn(){
+  
+  rand = Math.floor(Math.random()*2);  //乱数作成
+ 
+  if (rand == 0) {
+    var text = "あんまんを食べよう！";
+    var img = '<img src="img/chukaman2_anko.png">';
+  }else {
+    var text = "肉まんを食べよう！";
+    var img = '<img src="img/chukaman1_niku.png">';  
+  }
+
+  //ボタンの文字を書き換え
+  btn.innerHTML = "もう一回決める" ;
+
+  random_text.innerHTML = text
+  random_img.innerHTML = img
+}
